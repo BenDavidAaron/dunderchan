1d9aea2abb0a793c95840c2109521de81c0d8c5f
Ben Aaron <ben@theaarons.com>
2023-07-11 20:41:36 -0700

    paint it black

--- app/main.py
+++ app/main.py
@@ -10,13 +10,16 @@ from app.models import database, poasts
 
 templates = Jinja2Templates(directory="templates")
 
+
 class CreatePoastForm(wtforms.Form):
     poast_text = wtforms.TextAreaField("text", [wtforms.validators.DataRequired()])
     reply_to = wtforms.HiddenField("reply_to")
     submit = wtforms.SubmitField("Submit")
 
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.on_event("startup")
 async def startup():
@@ -33,19 +36,22 @@ async def shutdown():
 @app.get("/threads")
 async def get_threads(request: Request) -> HTMLResponse:
     new_form = CreatePoastForm()
-    new_form.reply_to.data = 'Nobody'
+    new_form.reply_to.data = "Nobody"
 
-    query = poasts.select().where(poasts.c.reply_to == None).order_by(poasts.c.id.desc())
+    query = (
+        poasts.select().where(poasts.c.reply_to == None).order_by(poasts.c.id.desc())
+    )
     results = await database.fetch_all(query=query)
 
     return templates.TemplateResponse(
-        "index.html", 
+        "index.html",
         {
             "request": request,
             "poasts": results,
             "form": new_form,
-        }
+        },
     )
+
 
 @app.get("/poast")
 async def redirect_to_index() -> RedirectResponse:
@@ -53,10 +59,14 @@ async def redirect_to_index() -> RedirectResponse:
 
 
 @app.post("/poast")
-async def create_poast(poast_text: Annotated[str, Form()], reply_to: Annotated[str, Form()], request: Request) -> RedirectResponse:
+async def create_poast(
+    poast_text: Annotated[str, Form()],
+    reply_to: Annotated[str, Form()],
+    request: Request,
+) -> RedirectResponse:
     print(request)
     # insert the poast into the database
-    if reply_to == 'Nobody':
+    if reply_to == "Nobody":
         reply_to = None
     query = poasts.insert().values(text=poast_text, reply_to=reply_to)
     await database.execute(query=query)
@@ -67,7 +77,11 @@ async def create_poast(poast_text: Annotated[str, Form()], reply_to: Annotated[s
 async def get_poast(poast_id: int, request: Request) -> HTMLResponse:
     # Get Poast and Replyguys
     poast_query = poasts.select().where(poasts.c.id == poast_id)
-    replies_query = poasts.select().where(poasts.c.reply_to == poast_id).order_by(poasts.c.id.desc())
+    replies_query = (
+        poasts.select()
+        .where(poasts.c.reply_to == poast_id)
+        .order_by(poasts.c.id.desc())
+    )
     the_poast = await database.fetch_one(query=poast_query)
     the_replies = await database.fetch_all(query=replies_query)
     # Prep form for new replyguy
