b30a8044db30409bb7eeb54cede7194c50b0cb5a
Ben Aaron <ben@theaarons.com>
2023-08-26 15:06:33 -0700

    running thru tutorial

--- app/main.py
+++ app/main.py
@@ -1,54 +1,18 @@
-import pathlib
-from typing import Annotated
-import wtforms
+from fastapi import FastAPI
 
-from fastapi import FastAPI, Form, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from fastapi_htmx import htmx, htmx_init
+from app.config import Settings
+from app.routes import router
 
-from app.models import database, posts
+settings = Settings()
 
+def get_app() -> FastAPI:
+    """FastAPI application factory with specified settings."""
+    new_app = FastAPI(**settings.fastapi_kwargs)
+    new_app.include_router(router)
+    return new_app 
 
-class CreatePoastForm(wtforms.Form):
-    poast_text = wtforms.TextAreaField("text", [wtforms.validators.DataRequired()])
-    submit = wtforms.SubmitField("Submit")
+app = get_app()
 
-
-app = FastAPI()
-htmx_init(templates=Jinja2Templates(directory=pathlib.Path("templates")))
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-@app.on_event("startup")
-async def startup():
-    await database.connect()
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
-
-
-@app.get("/", response_class=HTMLResponse)
-@htmx("index", "index")
-async def root_page(request: Request) -> HTMLResponse:
-    recent_thread_query = posts.select().order_by(posts.c.id.desc()).limit(7)
-    recent_threads = await database.fetch_all(query=recent_thread_query)
-    return {
-        "motd": "Hello World",
-        "form": CreatePoastForm(),
-        "threads": recent_threads,
-    }
-
-
-@app.get("/thread/{id}", response_class=HTMLResponse)
-@htmx("thread", "thread")
-async def thread_page(request: Request, id: int) -> HTMLResponse:
-    thread_query = posts.select().where(posts.c.id == id)
-    thread = await database.fetch_one(query=thread_query)
-    return {
-        "thread": thread,
-        "form": CreatePoastForm(),
-    }
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, **settings.uvicorn_kwargs)
