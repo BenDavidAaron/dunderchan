e737993e2c8bfdf3e5506b7bcf05243c46714f44
Ben Aaron <ben@theaarons.com>
2023-08-26 17:44:11 -0700

    Handle new Poast Submissions

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 """Dumbapp"""
 from pathlib import Path
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi_htmx import htmx, htmx_init
+from pydantic import BaseModel
 
 app = FastAPI()
 htmx_init(templates=Jinja2Templates(directory=Path("app") / "templates"))
+
+data = {"posts": []}
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -17,14 +20,28 @@ async def root_page(request: Request):
     return {"greeting": "Hello World"}
 
 
+class PostCreateSchema(BaseModel):
+    title: str
+    content: str
+
+
+class PostSchema(PostCreateSchema):
+    id: int
+
+
 @app.get("/posts", response_class=HTMLResponse)
 @htmx("posts", "posts")
 async def get_posts(request: Request):
     """Get posts"""
-    return {
-        "posts": [
-            {"id": 1, "title": "Post 1", "body": "Body 1"},
-            {"id": 2, "title": "Post 2", "body": "Body 2"},
-            {"id": 3, "title": "Post 3", "body": "Body 3"},
-        ]
-    }
+    return data
+
+
+@app.post("/posts", response_class=HTMLResponse)
+@htmx("posts", "posts")
+async def create_post(
+    request: Request, title: str = Form(...), content: str = Form(...)
+):
+    """Create a new post"""
+    new_post = {"id": len(data["posts"]) + 1, "title": title, "content": content}
+    data["posts"].append(new_post)
+    return data
