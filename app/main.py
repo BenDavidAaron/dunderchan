6b2e84ddb951f796199ed16417ee487b584e9a60
Ben Aaron <ben@theaarons.com>
2023-08-26 17:57:53 -0700

    Add database

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 """Dumbapp"""
+from os import environ
 from pathlib import Path
 
+import sqlalchemy
+from databases import Database
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -10,7 +13,32 @@ from pydantic import BaseModel
 app = FastAPI()
 htmx_init(templates=Jinja2Templates(directory=Path("app") / "templates"))
 
-data = {"posts": []}
+DATABASE_URL = environ.get("DUNDERCHAN_SQL_URL", "sqlite:///./test.db")
+database = Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+posts = sqlalchemy.Table(
+    "poasts",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True, autoincrement=True),
+    sqlalchemy.Column("title", sqlalchemy.String, nullable=False),
+    sqlalchemy.Column("content", sqlalchemy.String, nullable=False),
+)
+
+metadata.create_all(engine)
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -33,7 +61,9 @@ class PostSchema(PostCreateSchema):
 @htmx("posts", "posts")
 async def get_posts(request: Request):
     """Get posts"""
-    return data
+    query_statement = posts.select().order_by(posts.c.id.desc()).limit(10)
+    posts_list = await database.fetch_all(query_statement)
+    return {"posts": posts_list}
 
 
 @app.post("/posts", response_class=HTMLResponse)
@@ -42,6 +72,12 @@ async def create_post(
     request: Request, title: str = Form(...), content: str = Form(...)
 ):
     """Create a new post"""
-    new_post = {"id": len(data["posts"]) + 1, "title": title, "content": content}
-    data["posts"].append(new_post)
-    return data
+    del request
+    insert_statement = posts.insert().values(
+        title=title,
+        content=content,
+    )
+    await database.execute(insert_statement)
+    query_statement = posts.select().order_by(posts.c.id.desc()).limit(10)
+    posts_list = await database.fetch_all(query_statement)
+    return {"posts": posts_list}
